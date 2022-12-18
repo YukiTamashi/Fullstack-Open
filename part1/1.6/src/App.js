ca35029f9e11b1437f9ab4d97875e0d81fa6a8df
Yukinori <38091245+YukiTamashi@users.noreply.github.com>
2022-12-18 16:53:42 +0000

    1.8

--- part1/1.6/src/App.js
+++ part1/1.6/src/App.js
@@ -5,7 +5,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const plusGood = () => (setGood(good + 1))
 
   return (
     <div>
@@ -13,10 +12,7 @@ const App = () => {
       <Button event ={() => setGood(good + 1)} text = {"good"} />
       <Button event ={() => setNeutral(neutral + 1)} text = {"neutral"} />
       <Button event ={() => setBad(bad + 1)} text = {"bad"} />
-      <h2>statistics</h2>
-      <Display good={good} />
-      <Display neutral={neutral} />
-      <Display bad={bad} />
+      <Statistics good ={good} neutral = {neutral} bad = {bad} />
     </div>
   )
 }
@@ -29,6 +25,21 @@ const Display = (props) => {
   for (let key in props){ 
   return <p>{key} {props[key]}</p>
   }
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad;
+  const average = (good - bad)/total
+  const positive = good/total;
+  return <div>
+    <h2>statistics</h2>
+    <Display good={good} />
+    <Display neutral={neutral} />
+    <Display bad={bad} />
+    <Display total={total} />
+    <Display average={average} />
+    <Display positive={positive + " %"} />
+  </div>
 }
 
 export default App
