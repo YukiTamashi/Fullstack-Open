5b8836025e6c93a9c7eda4c98f35a6d5233ca64d
Yukinori <38091245+YukiTamashi@users.noreply.github.com>
2022-12-19 21:28:27 +0000

    2.7

--- part2/Phonebook/src/App.js
+++ part2/Phonebook/src/App.js
@@ -2,14 +2,21 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', 
+      id: 1,
+    }
   ]) 
   const [newName, setNewName] = useState('')
 
   const handleName = (e) => setNewName(e.target.value)
   const addPerson = (e) => {
     e.preventDefault();
-    setPersons(persons.concat(newName));
+    if (persons.some(person => IncludesInsensitive(person.name, newName))){
+      alert(`${newName} already on the list`);
+    }
+    else{
+      setPersons(persons.concat({name: newName, id: persons.length + 1}));
+    }
     setNewName('');
   }
 
@@ -18,16 +25,30 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleName}/>
+          Name: <input value={newName} onChange={handleName}/>
         </div>
         <div>
           <button onClick={addPerson} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <List persons={persons} />
     </div>
   )
 }
+
+const List = ({persons}) => (
+  <ul>
+  {persons.map((person)=><Number key={person.id} person={person} />)}
+  </ul>
+)
+
+const Number =({person}) => (
+  <li>{person.name}</li>
+)
+
+const IncludesInsensitive = (toCompare, compared) => (
+  toCompare.toLowerCase() === compared.toLowerCase()
+)
 
 export default App
